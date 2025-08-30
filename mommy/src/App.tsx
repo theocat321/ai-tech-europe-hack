3720cc84e49143acce9ccf4fbed50af3badc102b
Theocat321 <adamo.mark25@gmail.com>
2025-08-30 12:52:01 +0100

    created personal context route

--- mommy/src/App.tsx
+++ mommy/src/App.tsx
@@ -1,14 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import HomePage from '@/pages/HomePage'
 import ChatPage from '@/pages/ChatPage'
+import AboutMePage from '@/pages/AboutMePage'
 
 function App() {
   return (
     <Router>
       <div className="w-full min-h-screen">
+        <nav className="p-4 bg-gray-100 dark:bg-gray-800">
+          <Link to="/" className="mr-4">Home</Link>
+          <Link to="/about-me">About Me</Link>
+        </nav>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/chat" element={<ChatPage />} />
+          <Route path="/about-me" element={<AboutMePage />} />
         </Routes>
       </div>
     </Router>
